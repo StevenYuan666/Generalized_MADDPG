feba4750c51537b0b43b34740f82e32b4d28511b
StevenYuan666 <ye.yuan3@mail.mcgill.ca>
2022-08-08 17:08:42 -0700

    Update MPE setting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from runner import Runner
 from common.arguments import get_args
-from common.utils import make_env
+from common.utils import make_env, make_overcook_env
 import numpy as np
 import random
 import torch
@@ -10,8 +10,12 @@ import matplotlib
 if __name__ == '__main__':
     # get the params
     args = get_args()
-    env, args = make_env(args)
+    env, args = make_overcook_env(args)
     runner = Runner(args, env)
+    seed = [0, 1, 2, 3, 4]
+    torch.manual_seed(0)
+    random.seed(0)
+    np.random.seed(0)
     matplotlib.use("Agg") # avoid "fail to allcoate bitmap"
     if args.evaluate:
         returns = runner.evaluate()
