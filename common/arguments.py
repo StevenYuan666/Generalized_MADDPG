3fccc6512b41825c69cc0451eecfdb7c11174929
StevenYuan666 <ye.yuan3@mail.mcgill.ca>
2022-08-08 23:57:46 -0700

    Update arguments.py

--- common/arguments.py
+++ common/arguments.py
@@ -9,9 +9,9 @@ Here are the param for the training
 def get_args():
     parser = argparse.ArgumentParser("Reinforcement Learning experiments for multiagent environments")
     # Environment
-    parser.add_argument("--scenario-name", type=str, default="asymmetric_advantages", help="name of the scenario script")
-    parser.add_argument("--max-episode-len", type=int, default=500, help="maximum episode length")
-    parser.add_argument("--time-steps", type=int, default=500000, help="number of time steps")
+    parser.add_argument("--scenario-name", type=str, default="simple_test", help="name of the scenario script")
+    parser.add_argument("--max-episode-len", type=int, default=100, help="maximum episode length")
+    parser.add_argument("--time-steps", type=int, default=2000000, help="number of time steps")
     # 一个地图最多env.n个agents，用户可以定义min(env.n,num-adversaries)个敌人，剩下的是好的agent
     parser.add_argument("--num-adversaries", type=int, default=0, help="number of adversaries")
 
